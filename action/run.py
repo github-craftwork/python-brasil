56b0d67e6f57337dc2c8d4c9b9d911691e284bb4
Brian Douglas <bdougie@users.noreply.github.com>
2019-10-23 08:41:46 -0300

    Update run.py

--- action/run.py
+++ action/run.py
@@ -2,8 +2,8 @@ import os
 import json
 import requests
 
-event = json.loads(open(os.getenv('GITHUB_EVENT_PATH'), "r"))
-print(event.read())
+# event = json.loads(open(os.getenv('GITHUB_EVENT_PATH'), "r"))
+# print(event.read())
 
 # comment = event["comment"]["body"]
 comment = ".invite me"
