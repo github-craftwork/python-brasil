7e64d056c51f4b701172055c36c7eb37f688b136
Brian Douglas <bdougie@users.noreply.github.com>
2019-10-23 06:48:13 -0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,33 @@
+import os
+import json
+import requests
+
+# event = json.loads(open(os.getenv('GITHUB_EVENT_PATH')))
+# print event 
+
+# comment = event["comment"]["body"]
+comment = ".invite me"
+team_id = 3414353
+# commenter = event["comment"]["user"]["login"]
+commenter = "notBdougie"
+
+API_ENDPOINT = "https://api.github.com/organizations/55258788/public_members/" + commenter
+API_ENDPOINT2 = "https://api.github.com/organizations/55258788/" + commenter
+
+print("-------------------------------------------------")
+
+
+if ".invite" in comment and len(comment.split()) == 2:
+    r = requests.get(url = API_ENDPOINT)
+
+    if r.status_code == 200:
+        exit(78)
+
+    headers={'Authorization': os.getenv('GITHUB_TOKEN')}
+
+    h = requests.post(url = API_ENDPOINT2, headers = headers)
+    print(h.text)
+
+print("-------------------------------------------------")
+
+print("Action succesfully ran")
